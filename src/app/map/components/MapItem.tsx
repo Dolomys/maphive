0bcef1e98fde2cfde67110440ea3c625c43f094f
Alex F <alexandreflo@hotmail.fr>
2025-02-26 23:05:31 +0100

    fix: details cards

--- src/app/map/components/MapItem.tsx
+++ src/app/map/components/MapItem.tsx
@@ -16,14 +16,14 @@ export const MapItem = ({ activity, onItemClick }: MapItemProps) => {
 
   return (
     <Card
-      className="w-full hover:shadow-lg transition-shadow cursor-pointer max-h-[200px] overflow-hidden "
+      className="w-full hover:shadow-lg transition-shadow cursor-pointer max-h-[220px] overflow-hidden"
       onClick={handleClick}
     >
       <CardHeader>
         <CardTitle className="text-xl font-bold">{activity.title}</CardTitle>
         <CardDescription className="text-md font-medium text-muted-foreground">{activity.subtitle}</CardDescription>
       </CardHeader>
-      <CardContent className="space-y-2">
+      <CardContent className="space-y-2 pb-4">
         <div className="flex items-center gap-2 text-sm">
           <MapPinIcon className="w-4 h-4" />
           <span>{activity.address?.street}</span>
@@ -32,7 +32,7 @@ export const MapItem = ({ activity, onItemClick }: MapItemProps) => {
           <AtSign className="w-4 h-4" />
           <span>{activity.contact}</span>
         </div>
-        <p className="text-sm text-muted-foreground mt-2 text-ellipsis">{activity.description}</p>
+        <p className="text-sm text-muted-foreground mt-2 line-clamp-2 overflow-hidden">{activity.description}</p>
       </CardContent>
     </Card>
   );
